f84d5463ea8231a105929c186a41cc7eebcc25f9
Bjørnar Hoff <bjornar_hoff@hotmail.no>
2018-09-12 10:33:16 +0200

    Gjort oppgave 8, og plassert inn testversjonen

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -7,11 +7,10 @@ import java.util.Random;
 public class Oblig1 {
 
 
-    public static void skriv (int [] a)         //Skriver ut tabellen
+    public static void skriv(int[] a)         //Skriver ut tabellen
     {
         System.out.println("Tabellen er nå: ");
-        for (int item:a)
-        {
+        for (int item : a) {
             System.out.print(item + ", ");
         }
         System.out.println("\n");
@@ -22,17 +21,16 @@ public class Oblig1 {
         Random r = new Random();
         int[] a = new int[n];
 
-        Arrays.setAll(a, i -> i +1);
+        Arrays.setAll(a, i -> i + 1);
 
-        for(int k = n -1; k > 0; k--)
-        {
-            int i = r.nextInt(k+1);
-            bytt(a,k,i);
+        for (int k = n - 1; k > 0; k--) {
+            int i = r.nextInt(k + 1);
+            bytt(a, k, i);
         }
         return a;
     }
 
-    public static void bytt(int [] a, int x, int y)     // Bytter om to verdier, x og y, i tabllen a
+    public static void bytt(int[] a, int x, int y)     // Bytter om to verdier, x og y, i tabllen a
     {
         int temp = a[x];
         a[x] = a[y];
@@ -40,61 +38,58 @@ public class Oblig1 {
     }
 
     // Det blir gjort (n-1) sammenligninger
-    public static int maks(int [] a)                // Sammenligner to nærliggende verdier og om den til venstre er størst, bytter plass
+    public static int maks(int[] a)                // Sammenligner to nærliggende verdier og om den til venstre er størst, bytter plass
     {
 
-    if (a == null)
-    {
-    throw new NoSuchElementException("Tabellen er tom"); // Kaster en feilmelding om tabellen er tom
-    }
-
-    for (int i = 0; i < a.length -1; i++)           // Bruker a.length -1 fordi antall verdier i arrayet er en større en siste index
-    {
-
-        if(a[i]> a[i+1])
-        {
-            // Det blir gjort flest ombytninger når tabellen er sortert i synkende rekkefølge
-            // Det blir gjort færrest ombytninger når tabellen er sortert i stigende rekkefølge
-            bytt(a,i, i+1);
-            System.out.println("Bytter om " + a[i] + " og " + a[i+1] + "\n");
+        if (a.length == 0) {
+            throw new NoSuchElementException("Tabellen er tom"); // Kaster en feilmelding om tabellen er tom
         }
-    }
-        return a[a.length-1];
+
+        for (int i = 0; i < a.length - 1; i++)           // Bruker a.length -1 fordi antall verdier i arrayet er en større en siste index
+        {
+
+            if (a[i] > a[i + 1]) {
+                // Det blir gjort flest ombytninger når tabellen er sortert i synkende rekkefølge
+                // Det blir gjort færrest ombytninger når tabellen er sortert i stigende rekkefølge
+                bytt(a, i, i + 1);
+                System.out.println("Bytter om " + a[i] + " og " + a[i + 1] + "\n");
+            }
+        }
+        return a[a.length - 1];
     }
 
 
     // Vi kan finne gjennomsnittet av antall ombytninger ved å summere antall ombytninger delt på antall kjøringer.
     // Ved 10 kjøringer av metoden, med et array med lengde 10, ble det i gjennomsnitt 7,3 ombytninger.
-    /**  Kan  du  på  grunnlag  av  dette  si  om  metodenmakser  bedre  (eller dårligere) enn de maks-metodene vi har sett på tidligere? */
-    public static int ombytninger(int [] a)
-    {
-        if (a == null)
-        {
+
+    /**
+     * Kan  du  på  grunnlag  av  dette  si  om  metodenmakser  bedre  (eller dårligere) enn de maks-metodene vi har sett på tidligere?
+     */
+    public static int ombyttinger(int[] a) {
+        if (a.length == 0) {
             throw new NoSuchElementException("Tabellen er tom"); // Kaster en feilmelding om tabellen er tom
         }
 
-        int antOmbytninger=0;
-        for (int i = 0; i < a.length -1; i++)           // Bruker a.length -1 fordi antall verdier i arrayet er en større en siste index
+        int antOmbytninger = 0;
+        for (int i = 0; i < a.length - 1; i++)           // Bruker a.length -1 fordi antall verdier i arrayet er en større en siste index
         {
 
-            if(a[i]> a[i+1])
-            {
+            if (a[i] > a[i + 1]) {
                 // Det blir gjort flest ombytninger når tabellen er sortert i synkende rekkefølge
                 // Det blir gjort færrest ombytninger når tabellen er sortert i stigende rekkefølge
-                bytt(a,i, i+1);
+                bytt(a, i, i + 1);
                 antOmbytninger++;   // Teller antall ombytninger
-                System.out.println("Bytter om " + a[i] + " og " + a[i+1] + "\n");
+                System.out.println("Bytter om " + a[i] + " og " + a[i + 1] + "\n");
             }
         }
         return antOmbytninger;
     }
 
 
+    // OPPGAVE 2
 
-                                    // OPPGAVE 2
-
-    public static int antallUlikeSortert(int [] a) {
-        int teller=1;                                  // Teller satt til 1 fordi første verdi vil alltid være unik
+    public static int antallUlikeSortert(int[] a) {
+        int teller = 1;                                  // Teller satt til 1 fordi første verdi vil alltid være unik
         for (int i = 0; i < a.length - 1; i++) {        // Looper gjennom lengden på tabellen
             if (a[i] > a[i + 1]) {                      // Sjekker om tabellen er sortert
                 throw new IllegalStateException("Tabellen er ikke sortert" + "\n");     // Gir tilbakemelding om den ikke er sortert
@@ -102,24 +97,24 @@ public class Oblig1 {
             if (a.length == 0) {        // Sjekker om tabellen er tom, hvis den er tom returner den 0
                 return 0;
             }
-            for(i=0;i<a.length-1;i++)          // Looper igjennom tabellen
+            for (i = 0; i < a.length - 1; i++)          // Looper igjennom tabellen
             {
-                if(a[i]!=a[i+1])               // Sjekker om to nærliggende verdier er ulik
+                if (a[i] != a[i + 1])               // Sjekker om to nærliggende verdier er ulik
                 {
                     teller++;                   // Om de er ulik, øker telleren med 1
                 }
             }
 
-        } return teller;
+        }
+        return teller;
     }
 
     // Metode som lager en tilfeldig tabell med tilfeldige verdier
-    public static int[] randomArray(int a)
-    {
+    public static int[] randomArray(int a) {
         int[] list = new int[a];            // Lager en liste med størrelse 'a'
-         for (int i=0; i<a; i++)            // Looper gjennom lengden på tabellen
+        for (int i = 0; i < a; i++)            // Looper gjennom lengden på tabellen
         {
-            int n = (int)(Math.random()*9 + 1); // Oppretter tilfeldige verdier
+            int n = (int) (Math.random() * 9 + 1); // Oppretter tilfeldige verdier
             list[i] = n;                        // Setter verdiene inn i tabellen
 
             System.out.print(list[i] + ", ");
@@ -129,17 +124,14 @@ public class Oblig1 {
     }
 
 
-
-    
-    public static void bubbleSort(int [] a)
-    {
+    public static void bubbleSort(int[] a) {
         for (int n = a.length; n > 1; n--) // For å vite antallet ganger løkken skal kjøre
         {
-            for (int i=1; i<n; i++)        // Kjøre om i er mindre enn antallet
+            for (int i = 1; i < n; i++)        // Kjøre om i er mindre enn antallet
             {
-                if (a[i-1]>a[i])           // Sjekker om verdien foran er større enn neste
+                if (a[i - 1] > a[i])           // Sjekker om verdien foran er større enn neste
                 {
-                    bytt(a,i-1,i);      // Bytter plass om verdien er større
+                    bytt(a, i - 1, i);      // Bytter plass om verdien er større
                 }
             }
         }
@@ -160,8 +152,7 @@ public class Oblig1 {
 
     }
 
-   public static int partition1(int arr[], int left, int right)
-    {
+    public static int partition1(int arr[], int left, int right) {
         int i = left, j = right;
         int tmp;
         int pivot = arr[(left + right) / 2];
@@ -185,52 +176,46 @@ public class Oblig1 {
     }
 
 
-   public static int partition(int a[], int v, int h, int skilleverdi)
-   {
-       while (true)
-    {
-        while (v<= h && a[h] % 2 < skilleverdi)
-        {
-            h--;
+    public static int partition(int a[], int v, int h, int skilleverdi) {
+        while (true) {
+            while (v <= h && a[h] % 2 < skilleverdi) {
+                h--;
 
+            }
+            while (v <= h && a[v] % 2 >= skilleverdi) {
+                v++;
+            }
+            if (v < h) {
+                bytt(a, v++, h--);
+            } else return v;
         }
-        while (v<= h && a[v] % 2 >= skilleverdi)
-        {
-            v++;
-        }
-        if (v<h)
-        {
-            bytt(a,v++,h--);
-        }
-    else return v;
     }
-   }
 
 
-
-                                    // OPPGAVE 3
-    public static int antallUlikeUSortert(int []a) {
+    // OPPGAVE 3
+    public static int antallUlikeUsortert(int[] a) {
         int different = 0;
 
-        for (int i = 1; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++) {
             boolean same = false;
-            for (int j=0; j<i; j++) {
+            for (int j = 0; j < i; j++) {
                 if (a[i] == a[j]) {
                     same = true;
                     break;
                 }
-                 }   if(!same) {
-                    different++;
-                    System.out.print(a[i] + ", ");
-                }
             }
+            if (!same) {
+                different++;
+                System.out.print(a[i] + ", ");
+            }
+        }
         return different;
     }
 
 
-                                // OPPGAVE 4
+    // OPPGAVE 4
 
-    public static void delsortering(int [] a) {
+    public static void delsortering(int[] a) {
 
         int partall = 0;
         int oddetall = 0;
@@ -240,53 +225,167 @@ public class Oblig1 {
         for (int i = 0; i < a.length; i++) {
 
             if (a[i] % 2 == 0) {
-                partall = partall +1;
+                partall = partall + 1;
+            } else {
+                oddetall = oddetall + 1;
             }
-            else
-                {
-                    oddetall = oddetall +1;
-                }
 
 
-
-    }
+        }
         System.out.println(Arrays.toString(a));
-        quickSort(a,0,oddetall-1);
-        quickSort(a,oddetall,a.length-1);
+        quickSort(a, 0, oddetall - 1);
+        quickSort(a, oddetall, a.length - 1);
         System.out.println(Arrays.toString(a));
 
     }
 
 
-                            // OPPGAVE 5
+    // OPPGAVE 5
 
     public static void rotasjon(char[] a) {
         int n = a.length;
         int d = 1;
 
-        if (n == 1) {
+        if (n < 1) {
             return;
         }
 
-        if ((d = d%n) < 0) {
+        if ((d = d % n) < 0) {
             d += n;
         }
 
         char[] b = Arrays.copyOfRange(a, n - d, n);     // Oppretter en hjelpetabell
 
         for (int i = n - 1; i >= d; i--) {
-            a[i] = a[i - d]; }                                 // Forskyver bokstavene en plass til høyre
+            a[i] = a[i - d];
+        }                                 // Forskyver bokstavene en plass til høyre
 
-            System.arraycopy(b, 0, a, 0, d);                      // Kopierer arrayet
+        System.arraycopy(b, 0, a, 0, d);                      // Kopierer arrayet
 
+    }
+
+
+    // OPPGAVE 6
+    public static void rotasjon(char[] a, int k) {
+        int n = a.length;
+
+        if (n < 1) {
+            return;
+        }
+
+        if ((k = k % n) < 0) {
+            k += n;
+        }
+
+        char[] b = Arrays.copyOfRange(a, n - k, n);     // Oppretter en hjelpetabell
+
+        for (int i = n - 1; i >= k; i--) {
+            a[i] = a[i - k];
+        }                                 // Forskyver bokstavene en plass til høyre
+
+        System.arraycopy(b, 0, a, 0, k);                      // Kopierer arrayet
+
+    }
+
+
+    //OPPGAVE8
+
+    public static int min(int[] a, int fra, int til) {
+        if (fra < 0 || til > a.length || fra >= til)
+            throw new IllegalArgumentException("Ulovlig intervall");
+
+        int f = fra;
+        int minverdi = a[fra];
+
+        for (int i = fra + 1; i < til; i++)
+            if (a[i] < minverdi) {
+
+                f = i; // Indeksen til minste verdi oppdateres
+                minverdi = a[f]; // Minste verdi oppdateres
+            }
+
+        return f; // Returnerer minste verdi
+    }
+
+
+    // METODE FOR Å SORTERE INDEKS MED ULIKE VERDIER
+
+    public static int[] indekssortering(int[] a) {
+        int[] indekstabell = new int[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            indekstabell[i] = i;
+        }
+
+
+        for (int i = 0; i < a.length - 1; i++) {
+            int indeks = min(a, i, a.length);
+            bytt(a, i, indeks);
+            bytt(indekstabell, i, indeks);
+        }
+
+
+        return indekstabell;
+    }
+
+
+                        // OPPGAVE 9
+
+    public static int[] tredjeMin(int[] a) {
+        int n = a.length;       // Lengden til tabellen
+
+        if (n < 3)     // Må ha minst 3 verdier
+        {
+            throw new NoSuchElementException("Tabellen er ugyldig. Må ha minst 3 verdier");
+        }
+
+        int m = 0;              // Posisjonen til den minste verdien
+        int nm = 1;             // Posisjonen til nest minste verdi
+        int tm = 2;             // Posisjonen til den tredje minste verdi
+
+        if (a[m] > a[nm])
+        {
+            m = 1;
+            nm = 0;
+        }
+
+        if (a[m] > a[tm])
+        {
+            int tmp = tm;
+            tm = m;
+            m = tmp;
+        }
+
+        if (a[nm] > a[tm])
+        {
+            int tmp = tm;
+            tm = nm;
+            nm = tmp;
+        }
+
+
+
+        int minVerdi = a[m];                // Minste verdi
+        int nestMinVerdi = a[nm];           // Nest minste verdi
+        int tredjeMinVerdi = a[tm];         // Tredje minste verdi
+
+
+        for (int i = 3; i < n; i++)
+        {
+
+        }
+
+        return new int[] {minVerdi, nestMinVerdi, tredjeMinVerdi};
     }
 
 
 
 
 
+
+
                
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
                                         // OPPGAVE 1
         //int [] a = randPerm(10);  // Lager en tilfeldig tabell
         //skriv(a);                   // Skriver ut tabellen før bytte
@@ -298,11 +397,11 @@ public class Oblig1 {
 
 
                                         // OPPGAVE 2
-/**
+
         int [] c = randomArray(10);      // Oppretter en tabell med tilfeldige tall
         quickSort(c,0,c.length-1);                      // Sorterer tabellen
         skriv(c);                           // Skriver ut den sorterte tabellen
-        System.out.println("Antall: " + antallUlikeSortert(c));     // Skriver ut antall ulike verdier i tabellen */
+        System.out.println("Antall: " + antallUlikeSortert(c));     // Skriver ut antall ulike verdier i tabellen
 
 
                                         // OPPGAVE 3
@@ -328,5 +427,5 @@ public class Oblig1 {
 
 
 
-    }
+    } */
 }
